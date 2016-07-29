5b95733d645d16034e7bdc394684d6fc996b778a
Thomas Miconi <thomas.miconi@gmail.com>
2016-07-29 18:38:51 -0400

    - Removing multipliers (now based solely on L1-norm of outgoing weights).
    - Applied to both lateral and feedforward weights.
    
    - No more `minimum norm' for clipping-from-below.
    
    - New figures, scripts, etc.

--- README.md
+++ README.md
@@ -1,22 +1,18 @@
 This is the source code for the arXiv preprint ["Neural networks with differentiable structure"](https://arxiv.org/abs/1606.06216). 
 
-This code implements a method whereby the  number of neurons in the network undergoes gradient descent, just like the weights of the
+This code implements recurrent neural networks with differentiable structure:  the  number of neurons in the network undergoes gradient descent, just like the weights of the
 network. The network adjusts its number of neurons to the complexity of the task at hand.
 
 This code is based on Andrej Karpathy's [`min-char-rnn.py`](https://gist.github.com/karpathy/d4dee566867f8291f086) program.
 
 `rnn.py` is the main program. You can run it "as is" (`python rnn.py`) to run
-the model on the "hard" problem for 300000 cycles.  It will generate an output
+the model on the "hard" problem for 100000 cycles.  It will generate an output
 file called `output.txt`, updated every 1000 cycles, which logs the current
 cycle number, position in the input file, loss, number of neurons, and total absolute sum of multipliers. (see code).
 
 Other
 python files in the repository generate inputs or figures, or submit jobs to a cluster.
 
-rnnAltern.py uses a different version of the algorithm, in which the
-multipliers are applied directly to the output of hidden neurons (and thus
-affect both feedforward and lateral/recurrent outputs). This wlao works, but
-produces somewhat lower performance.
 
 
 
--- rnn.py
+++ rnn.py
@@ -13,15 +13,19 @@ import sys
 
 # Global meta-parameters, modifiable by command line
 g = {
-'DIR' : '.',
-'NBSTEPS' : 300000,
-'COEFFMULTIPNORM' : 3e-5,
+'ADDDEL': 1,
+'ETA' : .01,
+'NBNEUR': 40,   # Number of neurons for fixed-size experiments (ignored if adddel is 1)
+'MAXDW': .01,  
+'DIR' : '.',  # The directory of input text files
+'NBSTEPS' : 100000,
+'COEFFWPEN' : 1e-4, 
 'EXPTYPE' : 'HARD',
 'DELETIONTHRESHOLD': .05,
-'MINMULTIP': .025,  # Must be lower than DELETIONTHRESHOLD !
+'MINMULTIP': .025,  # Must be lower than DELETIONTHRESHOLD ! NOTE: Has no effect in the current version of the code.
 'NBMARGIN' : 1,
-'PROBADEL': .25,
-'PROBAADD': .05,
+'PROBADEL': .05,
+'PROBAADD': .01,
 'RNGSEED' : 0
 }
 
@@ -30,14 +34,14 @@ g = {
 argpairs = [sys.argv[i:i+2] for i in range(1, len(sys.argv), 2)]
 for argpair in argpairs:
     if not (argpair[0] in g):
-        sys.exit("Error, tried to pass value of non-existent parameter "+argpair[0])
-    if argpair[0] == 'EXPTYPE':
+        raise Exception("Error, tried to pass value of non-existent parameter "+argpair[0])
+    if argpair[0] == 'EXPTYPE' or argpair[0] == 'DIR':
         g[argpair[0]] = argpair[1]
     else:
         g[argpair[0]] = float(argpair[1])
 
 if (g['EXPTYPE'] not in ['HARD', 'EASY', 'HARDEASY', 'EASYHARDEASY']):
-    sys.exit('Wrong EXPTYPE value')
+    raise Exception('Wrong EXPTYPE value')
 g['NBMARGIN'] = int(g['NBMARGIN'])
 g['RNGSEED'] = int(g['RNGSEED'])
 print g
@@ -60,18 +64,20 @@ ix_to_char = { i:ch for i,ch in enumerate(chars) }
 
 # hyperparameters
 MAX_HIDDEN_SIZE = 100 # Maximum size of hidden layer of neurons (same as fixed size in original min-char-rnn.py)
-hidden_size = 1 # size of hidden layer of neurons - start from 1 node.
+if g['ADDDEL']:
+    hidden_size = 1 # size of hidden layer of neurons - start from 1 node.
+else:
+    hidden_size = g['NBNEUR'] # fixed size
 seq_length = 40 # number of steps to unroll the RNN for 
-learning_rate = 1e-1
+learning_rate = g['ETA']
 
 # network parameters
 Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
 Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
-multips = .001 * np.ones((hidden_size, 1)); # multipliers 
-multips.fill(1.0) # Start with a multiplier of 1 on the single starting node.
-Wiy = np.random.randn(vocab_size, hidden_size)*0.01 # hidden (after multiplier) to output. See below
+Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden (after multiplier) to output. See below
 bh = np.zeros((hidden_size, 1)) # hidden bias
 by = np.zeros((vocab_size, 1)) # output bias
+normz = np.zeros_like(bh)
 
 ages = np.zeros(hidden_size) # Ages of all neurons. Not used at present.
 
@@ -81,7 +87,7 @@ def lossFun(inputs, targets, hprev):
   hprev is Hx1 array of initial hidden state
   returns the loss, gradients on model parameters, and last hidden state
   """
-  xs, hs, postmultips, ys, ps = {}, {}, {}, {}, {}
+  xs, hs, ys, ps = {}, {}, {}, {}
   hs[-1] = np.copy(hprev)
   loss = 0
   # forward pass
@@ -90,36 +96,28 @@ def lossFun(inputs, targets, hprev):
     xs[t][inputs[t]] = 1
 
     hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
-    postmultips[t] = multips * hs[t]  # "postmultip" is the output of the hidden layer after the multipliers, which is to be fed into y (through the Wiy weight matrix)
-    ys[t] = np.dot(Wiy, postmultips[t]) + by # unnormalized log probabilities for next chars
+    ys[t] = np.dot(Why, hs[t]) + by # unnormalized log probabilities for next chars
     ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
     loss += -np.log(ps[t][targets[t],0]) # softmax (cross-entropy loss)
   
   # backward pass: compute gradients going backwards
-  dWxh, dWhh, dmultips, dWiy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(multips), np.zeros_like(Wiy)
+  dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh),  np.zeros_like(Why)
   dbh, dby = np.zeros_like(bh), np.zeros_like(by)
   dhnext = np.zeros_like(hs[0])
   for t in reversed(xrange(len(inputs))):
     dy = np.copy(ps[t])
     dy[targets[t]] -= 1 # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
-    dWiy += np.dot(dy, postmultips[t].T) 
+    dWhy += np.dot(dy, hs[t].T) 
     dby += dy
-    dpostmultip = np.dot(Wiy.T, dy) # dE/dIntoY, as a function of dE/dy
-    
-    # Gradient to be applied to the multipliers
-    dmultips += (1.0 * dpostmultip * hs[t]  # This part descends the error gradient
-            + g['COEFFMULTIPNORM'] * np.sign(multips)) # L1-norm regularization. The derivative of abs(x) is sign(x) (though in practice multipliers are always positive due to clipping at non-zero positive value). 
-            # + .001 * multips) # This would add an L2-regularization term, which we don't use here.
-    
-    dh = dpostmultip * multips + dhnext
+    dh = np.dot(Why.T, dy) + dhnext
     dhraw = (1 - hs[t] * hs[t]) * dh # backprop through tanh nonlinearity
     dbh += dhraw
     dWxh += np.dot(dhraw, xs[t].T)
     dWhh += np.dot(dhraw, hs[t-1].T)
     dhnext = np.dot(Whh.T, dhraw)
-  for dparam in [dWxh, dWhh, dmultips, dWiy, dbh, dby]:
-    np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
-  return loss, dWxh, dWhh, dmultips, dWiy, dbh, dby, hs[len(inputs)-1]
+  #for dparam in [dWxh, dWhh,  dWhy, dbh, dby]:
+  #  np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients - clipping is actually done just before the update, after learning_rate has been applied - see below
+  return loss, dWxh, dWhh,  dWhy, dbh, dby, hs[len(inputs)-1]
 
 def sample(h, seed_ix, n):
   """ 
@@ -131,7 +129,7 @@ def sample(h, seed_ix, n):
   ixes = []
   for t in xrange(n):
     h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
-    y = np.dot(Wiy, multips * h) + by
+    y = np.dot(Why, h) + by
     p = np.exp(y) / np.sum(np.exp(y))
     ix = np.random.choice(range(vocab_size), p=p.ravel())
     x = np.zeros((vocab_size, 1))
@@ -140,8 +138,8 @@ def sample(h, seed_ix, n):
   return ixes
 
 n, p = 0, 0
-mWxh, mWhh, mmultips, mWiy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(multips), np.zeros_like(Wiy)
-mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
+mWxh, mWhh,  mWhy = .01 * np.ones_like(Wxh), .01 * np.zeros_like(Whh),  .01 * np.zeros_like(Why)
+mbh, mby = .01 * np.ones_like(bh), .01 * np.zeros_like(by) # memory variables for RMSProp
 smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
 
 
@@ -160,89 +158,113 @@ while True:
     print '----\n %s \n----' % (txt, )
 
   # forward seq_length characters through the net and fetch gradient
-  loss, dWxh, dWhh, dmultips, dWiy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
-  smooth_loss = smooth_loss * 0.999 + loss * 0.001
+  loss, dWxh, dWhh,  dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
+  smooth_loss = smooth_loss * 0.99 + loss * 0.01
   if n % 100 == 0: 
-      print 'iter %d, position in data %d, loss: %f , nb hidden neurons %d, sum-abs multips: %f' % (n, p, smooth_loss, hidden_size, sum(abs(multips))), # print progress
-      print multips.T
+      print 'iter %d, position in data %d, loss: %f , nb hidden neurons %d, sum-abs norms: %f' % (n, p, smooth_loss, hidden_size, sum(abs(normz))), # print progress
+      print normz.T
   if n % 1000 == 0: 
       with open("output.txt", "a") as myf:
-        msg = "%d %d %f  %d %f" % (n, p, smooth_loss, hidden_size, sum(abs(multips))) # print progress
+        msg = "%d %d %f  %d %f" % (n, p, smooth_loss, hidden_size, sum(abs(normz))) # print progress
         myf.write(msg+"\n")
  
 
   # perform parameter update with Adagrad
-  for param, dparam, mem in zip([Wxh, Whh, multips, Wiy, bh, by], 
-                                [dWxh, dWhh, dmultips, dWiy, dbh, dby], 
-                                [mWxh, mWhh, mmultips, mWiy, mbh, mby]):
-    mem += dparam * dparam
-    param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
-
-
-  # Neuron addition / deletion
-  # Deletable neurons are those whose multipliers fall below threshold.
-  # We want to delete excess below-threshold neurons, keeping only NBMARGIN below-threshold neuron at any time; or add one new neuron if no below-threshold neuron remains.
-
-  ages += 1
-
-  multips[multips < g['MINMULTIP']] = g['MINMULTIP']  # multipliers are clipped from below
   
-
-  # Which neurons are above threshold ('selected' for preservation) ?
-  sel = (abs(multips) > g['DELETIONTHRESHOLD'])[:,0] # | (ages < 500)
+  for param, dparam, mem in zip([Wxh, Whh,  Why, bh, by], 
+                                [dWxh, dWhh,  dWhy, dbh, dby], 
+                                [mWxh, mWhh,  mWhy, mbh, mby]):
+    # mem += dparam * dparam   # Adagrad
+    mem += .01 * (dparam * dparam - mem)  # RMSProp
+    RMSdelta = -learning_rate * dparam / np.sqrt(mem + 1e-8) # RMSProp update
+    np.clip(RMSdelta, -g['MAXDW'], g['MAXDW'], out = RMSdelta)  # Clipping the weight modifications
+    param += RMSdelta
+ 
+  # Note that 1-norm penalty on weights is applied even for fized-size! If you want to have no penalty, set COEFFWPEN to 0 (but this will decrease performance).
+  Why -= g['COEFFWPEN'] * np.sign(Why)
+  Whh -= g['COEFFWPEN'] * np.sign(Whh)
   
-  if sum(sel) < hidden_size - g['NBMARGIN'] :
-   
-    # Preserve 1-PROBADEL% of the below-threshold neurons, in addition to NBMARGIN below-threshold neurons (NBMARGIN is usually set to 1).
-    # (Perhaps select the most recent neurons for deletion? Future work.)
-    deletable = np.where(sel == False)[0]
-    np.random.shuffle(deletable)
-    for xx in range(g['NBMARGIN']):
-        sel[deletable[xx]] = True
-    deletable = deletable[g['NBMARGIN']:]
-    for x in deletable:
-        if np.random.rand() > g['PROBADEL']: # Note that this is a test for preservation rather than deletion, hence the >
-            sel[x] = True
+  # Computing the L1-norm of outgoing weights for each neuron.
+  # The norm of lateral weights is scaled by the number of neurons and multiplied by 4, so it should remain roughly similar to the norm of feedforward weights as the network changes size (there are 4 output neurons)
+  normz = .5 * (np.sum(np.abs(Why), axis = 0) + 4.0 * np.sum(np.abs(Whh), axis = 0) / hidden_size)
+
+  
+  if g['ADDDEL']:
 
 
-    # Delete all other deletable neurons
-    hidden_size = sum(sel)
-    Whh = Whh[sel,:][:, sel]
-    Wxh = Wxh[sel, :]
-    multips = multips[sel]
-    Wiy = Wiy[:, sel]
-    bh = bh[sel]
-    hprev = hprev[sel]
-    ages = ages[sel]
-    
-    mWxh = mWxh[sel, :]
-    mWhh = mWhh[sel,:][:, sel]
-    mmultips = mmultips[sel]
-    mWiy = mWiy[:, sel]
-    mbh = mbh[sel]
-    
-  if hidden_size < MAX_HIDDEN_SIZE -1:
-      if ( (sum((abs(multips) > g['DELETIONTHRESHOLD'])[:,0]) > hidden_size - g['NBMARGIN']) & (np.random.rand() < g['PROBAADD']))  \
-        | (np.random.rand() < 1e-4):
-      # Add a new neuron
-          Whh = np.append(Whh, np.random.randn(1, hidden_size)*0.01, axis=0)
-          Whh = np.append(Whh, np.random.randn(hidden_size+1, 1)*0.01, axis=1)
-          Wxh = np.append(Wxh, np.random.randn(1, vocab_size)*0.01, axis=0)
-          Wiy = np.append(Wiy, np.random.randn(vocab_size,1)*0.01, axis=1)
-          bh = np.append(bh, np.zeros((1,1)), axis=0)
-          hprev = np.append(hprev, np.zeros((1,1)), axis=0)
-          multips = np.append(multips,  g['DELETIONTHRESHOLD'] * np.ones((1,1)), axis=0)  # Initial multiplier for new neurons is set to deletion threshold
-          ages = np.append(ages, 0)
+      # Neuron addition / deletion
+      # Deletable neurons are those whose outgoing weights fall below a certain threshold in L1-norm.
+      # We want to delete excess below-threshold neurons, keeping only NBMARGIN below-threshold neuron at any time; or add one new neuron if no below-threshold neuron remains. (Both with a certain probability)
 
-          mWhh = np.append(mWhh, np.zeros((1, hidden_size)), axis=0)
-          mWhh = np.append(mWhh, np.zeros((hidden_size+1, 1)), axis=1)
-          mWxh = np.append(mWxh, np.zeros((1, vocab_size)), axis=0)
-          mWiy = np.append(mWiy, np.zeros((vocab_size,1)), axis=1)
-          mbh = np.append(mbh, np.zeros((1,1)), axis=0)
-          mmultips = np.append(mmultips, np.zeros((1,1)), axis=0)
+      ages += 1
 
-          hidden_size += 1
-          print "Adding Neuron"
+      #normz[normz < g['MINMULTIP']] = g['MINMULTIP']  # outgoing weight norms are clipped from below
+      
+
+      # Which neurons are above threshold ('selected' for preservation) ?
+      sel = abs(normz) > g['DELETIONTHRESHOLD']#[0] # | (ages < 500)
+      
+      if sum(sel) < hidden_size - g['NBMARGIN'] :
+       
+        # Preserve 1-PROBADEL% of the below-threshold neurons, in addition to NBMARGIN below-threshold neurons (NBMARGIN is usually set to 1).
+        # (Perhaps select the most recent neurons for deletion? Future work.)
+        deletable = np.where(sel == False)[0]
+        np.random.shuffle(deletable)
+        for xx in range(g['NBMARGIN']):
+            sel[deletable[xx]] = True
+        deletable = deletable[g['NBMARGIN']:]
+        for x in deletable:
+            if np.random.rand() > g['PROBADEL']: # Note that this is a test for preservation rather than deletion, hence the >
+                sel[x] = True
+
+
+        # Delete all other deletable neurons
+        hidden_size = sum(sel)
+        Whh = Whh[sel,:][:, sel]
+        Wxh = Wxh[sel, :]
+        normz = normz[sel]
+        Why = Why[:, sel]
+        bh = bh[sel]
+        hprev = hprev[sel]
+        ages = ages[sel]
+        
+        mWxh = mWxh[sel, :]
+        mWhh = mWhh[sel,:][:, sel]
+        mWhy = mWhy[:, sel]
+        mbh = mbh[sel]
+        
+
+      # Addition of new neurons, if appropriate:
+      if hidden_size < MAX_HIDDEN_SIZE -1:
+          if ( (sum((abs(normz) > g['DELETIONTHRESHOLD'])) > hidden_size - g['NBMARGIN']) & (np.random.rand() < g['PROBAADD']))  \
+            | (np.random.rand() < 1e-4):
+
+              Whh = np.append(Whh, np.random.randn(1, hidden_size)*0.01, axis=0)
+              Wxh = np.append(Wxh, np.random.randn(1, vocab_size)*0.01, axis=0)
+
+            # The (absolute values of) outgoing weights of the added neuron must sum to g['DELETIONTHRESHOLD']
+              newWhy = np.random.randn(vocab_size,1)
+              newWhy = .5 * g['DELETIONTHRESHOLD'] * newWhy / (1e-8 + np.sum(abs(newWhy)))
+              Why = np.append(Why, newWhy, axis=1)
+              
+              newWhh = np.random.randn(hidden_size+1, 1)
+              newWhh = .5 * hidden_size * g['DELETIONTHRESHOLD'] * newWhh / (1e-8 + 4.0 * np.sum(abs(newWhh)))
+              #newWhh *= .01
+              Whh = np.append(Whh, newWhh, axis=1)
+
+              bh = np.append(bh, np.zeros((1,1)), axis=0)
+              hprev = np.append(hprev, np.zeros((1,1)), axis=0)
+              #normz = np.append(normz,  g['DELETIONTHRESHOLD'] )  
+              ages = np.append(ages, 0)
+
+              mWhh = np.append(mWhh, .01 * np.ones((1, hidden_size)), axis=0)
+              mWhh = np.append(mWhh, .01 * np.ones((hidden_size+1, 1)), axis=1)
+              mWxh = np.append(mWxh, .01 * np.ones((1, vocab_size)), axis=0)
+              mWhy = np.append(mWhy, .01 * np.ones((vocab_size,1)), axis=1)
+              mbh = np.append(mbh, .01 * np.ones((1,1)), axis=0)
+
+              hidden_size += 1
+              print "Adding Neuron"
 
 
 
